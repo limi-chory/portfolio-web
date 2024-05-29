bffc756fe4b5474803bcb9f7a9bd2c9646928c47
USER <user@jjaeoliui-iPad.localdomain>
2024-05-29 20:18:42 +0900

    Add PageInfo, GetDiariesResponse, GetGuestBooksResponse DTO

--- src/dtos/diary.dto.ts
+++ src/dtos/diary.dto.ts
@@ -1,4 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Diaries } from 'src/entities/diaries.entity';
+import { PageInfo } from './common.dto';
 
 export class CreateDiaryDto {
   @ApiProperty({ description: '제목' })
@@ -14,4 +16,12 @@ export class UpdateDiaryDto {
 
   @ApiProperty({ description: '변경할 내용' })
   contents?: string;
+}
+
+export class GetDiariesResponse {
+  @ApiProperty({ type: () => Diaries, isArray: true })
+  data: Diaries[];
+
+  @ApiProperty()
+  pageInfo: PageInfo;
 }
--- src/dtos/guest_book.dto.ts
+++ src/dtos/guest_book.dto.ts
@@ -1,4 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Guest_Book } from 'src/entities/guest_books.entity';
+import { PageInfo } from './common.dto';
 
 export class CreateGuestBookDto {
   @ApiProperty({ description: '게스트 이름' })
@@ -20,4 +22,12 @@ export class UpdateGuestBookDto {
 
   @ApiProperty({ description: '변경할 내용' })
   contents?: string;
+}
+
+export class GetGuestBooksResponse {
+  @ApiProperty({ type: () => Guest_Book, isArray: true })
+  data: Guest_Book[];
+
+  @ApiProperty()
+  pageInfo: PageInfo;
 }
